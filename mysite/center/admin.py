c0b76b2fa329a34acd5a9d81f56f5b3d04445544
Celyne <dianakydd2@gmail.com>
2024-07-13 15:59:20 +0300

    Updated the models files of vaccine, center & storage

--- mysite/center/admin.py
+++ mysite/center/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from center.models import Center, Storage
 
-# Register your models here.
+admin.site.register(Center)
+admin.site.register(Storage)
--- mysite/center/models.py
+++ mysite/center/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from vaccine.models import Vaccine
 
 # Create your models here.
+class Center(models.Model):
+    name = models.CharField(max_length=124)
+    address = models.TextField(max_length=500)
+
+    def __str__(self):
+        return self.name
+    
+class Storage(models.Model):
+    center = models.ForeignKey(Center, on_delete=models.CASCADE)
+    vaccine = models.ForeignKey(Vaccine, on_delete=models.CASCADE)
+    total_quantity = models.IntegerField(default=0)
+    booked_quantity = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.center.name + " | " + self.vaccine.name
+
+
+

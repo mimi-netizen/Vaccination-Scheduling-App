63442f35bd3976bfbc94e42d596bcd85892a938b
Celyne <dianakydd2@gmail.com>
2024-07-13 15:18:26 +0300

    Added Sites

--- mysite/vaccine/admin.py
+++ mysite/vaccine/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from vaccine.models import Vaccine
 
-# Register your models here.
+admin.site.register(Vaccine)
--- mysite/vaccine/models.py
+++ mysite/vaccine/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Vaccine(models.Model):
+    name = models.CharField("Vaccine Name", max_length=32)
+    description = models.TextField(max_length=1024)
+    number_of_doses = models.IntegerField(default=1)
+    interval = models.IntegerField(default=0, help_text="Please provide interval in days")
+    storage_temperature = models.IntegerField(null=True, blank=True, help_text="Provide the temperature in Celcius")
+    minimum_age = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.name

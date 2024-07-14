a56508e50d5fd89ac1b45bd3fa1a9fc44dfdde22
Celyne <dianakydd2@gmail.com>
2024-07-14 13:47:50 +0300

    Updated campaign

--- mysite/campaign/admin.py
+++ mysite/campaign/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from campaign.models import Campaign
 
-# Register your models here.
+admin.site.register(Campaign)
--- mysite/campaign/models.py
+++ mysite/campaign/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from center.models import Center
+from vaccine.models import Vaccine
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Campaign(models.Model):
+    center = models.ForeignKey(Center, on_delete=models.CASCADE)
+    vaccine = models.ForeignKey(Vaccine, on_delete=models.CASCADE)
+    start_date = models.DateField(null=True)
+    end_date = models.DateField(null=True)
+    agents = models.ManyToManyField(User, blank=True)
+
+    def __str__(self):
+        return str(self.vaccine.name).upper() + " | " + str(self.center.name).upper()
+
+
+class Slot(models.Model):
+    campaign = models.ForeignKey(Campaign, on_delete=models.CASCADE)
+    date = models.DateField(null=True, blank=True)
+    start_time = models.TimeField(null=True, blank=True)
+    end_time = models.TimeField(null=True, blank=True)
+    max_capacity = models.IntegerField(default=0, null=True, blank=True)
+    reserved = models.IntegerField(default=0, null=True, blank=True)
+
+    def __str__(self):
+        return str(self.date) + " | " + str(self.start_time) + " to " + str(self.end_time)
